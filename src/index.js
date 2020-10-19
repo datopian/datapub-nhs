545861497789cb919c255de81bdf23f8b5db5e57
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-10-19 10:41:44 +0400

    [edit][s]: remove unnecessary codes

--- src/index.js
+++ src/index.js
@@ -1,48 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// Mount the ResourceEditor app explicitly
-// (make sure you call the function after it's loaded)
-function mountResourceEditorApp([elementId, config, resource] = ['root', {
-  authToken: null,
-  api: null,
-  lfs: null,
-  organizationId: null,
-  datasetId: null,
-  resourceId: null,
-}, {}]) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App config={config} resource={resource} />
-    </React.StrictMode>,
-    document.getElementById(elementId)
-  );
-};
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // Automatically mount the app if an element with id='ResourceEditor' exists
-const element = document.getElementById('ResourceEditor');
+const element = document.getElementById("ResourceEditor");
 if (element) {
   const config = {
-    datasetId: element.getAttribute('data-dataset-id'),
-    api: element.getAttribute('data-api'),
-    lfs: element.getAttribute('data-lfs'),
-    authToken: element.getAttribute('data-auth-token'),
-    organizationId: element.getAttribute('data-organization-id'),
-    resourceId: element.getAttribute('data-resource-id')
-  }
+    datasetId: element.getAttribute("data-dataset-id"),
+    api: element.getAttribute("data-api"),
+    lfs: element.getAttribute("data-lfs"),
+    authToken: element.getAttribute("data-auth-token"),
+    organizationId: element.getAttribute("data-organization-id"),
+    resourceId: element.getAttribute("data-resource-id"),
+  };
 
   ReactDOM.render(
     <React.StrictMode>
-        <App
-          config={config}
-          resource={ element.getAttribute('data-resource')}
-        />
+      <App config={config} resource={element.getAttribute("data-resource")} />
     </React.StrictMode>,
     element
   );
 }
-
-
-export { ResourceEditor } from './App';
