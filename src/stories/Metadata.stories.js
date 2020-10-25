22f17dd8273181f888f24bc951f72ae727ce7707
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-10-25 15:48:34 +0400

    [add][m]: Add story for Metadata comp

---
+++ src/stories/Metadata.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+import Metadata from "../components/Metadata";
+
+export default {
+  title: "Components /Metadata",
+  component: Metadata,
+  argTypes: {
+    loading: { control: "boolean" },
+  },
+};
+
+const Template = (args) => <Metadata {...args} />;
+
+export const Idle = Template.bind({});
+
+Idle.args = {
+  metadata: {
+    title: "",
+    description: "",
+    encoding: "",
+    path: "",
+    format: "",
+    license: "",
+  },
+  handleChange: action("change value"),
+  handleSubmit: action("submit form"),
+  uploadSuccess: false,
+};
+
+export const Loading = Template.bind({});
+
+Loading.args = {
+  ...Idle.args,
+};
+
+export const Success = Template.bind({});
+
+Success.args = {
+  ...Idle.args,
+  uploadSuccess: true,
+};
