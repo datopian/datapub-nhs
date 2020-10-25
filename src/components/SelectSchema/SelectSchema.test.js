5fcd5814d7c42fe44aeea551ff8a111b27bc4d59
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-10-25 14:50:56 +0400

    [select][m]: Create dropdown component to select resource from the list - Issue #5

---
+++ src/components/SelectSchema/SelectSchema.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import SelectSchema from ".";
+
+describe("<SelectSchema />", () => {
+  it("render SelectSchema without crashing", () => {
+    const onSchemaSelected = jest.fn();
+    const resources = [{ name: "sample.csv", id: "sample-id" }];
+    const wrapper = shallow(
+      <SelectSchema resources={resources} onSchemaSelected={onSchemaSelected} />
+    );
+
+    expect(wrapper.contains("Copy schema from existing resource")).toEqual(
+      true
+    );
+    expect(wrapper.contains("sample.csv")).toEqual(true);
+    expect(wrapper.find("select")).toHaveLength(1);
+    expect(wrapper.find("option")).toHaveLength(2);
+  });
+
+  it("Can select schema from the list", () => {
+    const onSchemaSelected = jest.fn();
+    const resources = [{ name: "sample.csv", id: "sample-id" }];
+    const wrapper = shallow(
+      <SelectSchema resources={resources} onSchemaSelected={onSchemaSelected} />
+    );
+
+    wrapper
+      .find("select")
+      .simulate("change", { target: { value: "sample-id" } });
+
+    expect(onSchemaSelected.mock.calls.length).toEqual(1);
+    // check if passes the resource id
+    expect(onSchemaSelected.mock.calls[0][0]).toBe("sample-id");
+  });
+});
---
+++ src/components/SelectSchema/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import PropTypes from "prop-types";
+
+const SelectSchema = ({ resources, client, onSchemaSelected }) => {
+  const [selectedSchema, setSelectedSchema] = useState("");
+
+  const onChangeHandlder = async (event) => {
+    const resourceId = event.target.value;
+    setSelectedSchema(event.target.value);
+    onSchemaSelected(resourceId);
+  };
+  return (
+    <div className="app-form-field">
+      <label className="metadata-label">
+        Copy schema from existing resource
+      </label>
+      <select
+        className="app-form-field-input"
+        value={selectedSchema}
+        onChange={onChangeHandlder}
+      >
+        <option value="">Select...</option>
+        {resources.map(({ name, schema, id }, index) => {
+          return (
+            <option
+              key={`select-schema-option-${id}-${index}`}
+              value={id}
+              disabled={!schema}
+            >
+              {name} {!schema ? "(no schema to copy)" : ""}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+SelectSchema.propTypes = {
+  resources: PropTypes.array.isRequired,
+  onSchemaSelected: PropTypes.func.isRequired,
+};
+
+export default SelectSchema;
